123ec17c59d738c0f6cfe85ece47c6e824857a20
Hidetake Iwata <int128@gmail.com>
2023-05-22 11:37:08 +0900

    Add arc-runner-set

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ kind create cluster --name arc-cluster
 kubectl version
 
 helmfile sync
+helm list -A
+kubectl get pods -n arc-systems
+kubectl rollout status deployment -n arc-systems arc-gha-runner-scale-set-controller
+
+sleep 300
